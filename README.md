9cef32bf304bc24de5ebffbf99962967c1bee853
bulatei <bulatei@list.ru>
2016-05-04 03:31:55 +0500

    4

--- README.md
+++ README.md
@@ -1,2 +1,165 @@
-# node-selectel-storage
-Upload and delete files in storage selectel
+# selectel-storage
+Пакет создан на базе selectel-manager для личного пользования.
+Буду рад если кому-то пригодится.
+
+Подробнее документация находится по адресу [Selectel Storage API](https://support.selectel.ru/storage/api_info/)
+
+## Установка
+Установка с помощью npm:
+
+    npm install --save selectel-storage
+
+
+## Начало
+
+```js
+var selectel = require('selectel-storage');
+selectel.setConf('login','password');
+```
+
+## Информация по хранилищу
+
+```js
+selectel.infoStorage(function(err, data){
+    /* Если ошибок нет, то err == null, data.success == true
+     * data.data тут заголовки ответа
+     * например:
+     * data.data['x-account-bytes-used'] - суммарный объём хранения данных в хранилище (в байтах)
+     */
+});
+```
+
+## Список контейнеров 
+
+```js
+selectel.listContainers({
+    format: 'json' //json или xml
+	//limit - число, ограничивает количество объектов в результате (по умолчанию 10000)
+	//marker - строка, результат будет содержать объекты по значению больше указанного маркера (полезно использовать для постраничной навигации и при большом количестве контейнеров)
+}, function (err , data) {
+	//Если ошибок нет, то err == null, data.success == true
+    //data.data - массив со списком контейнеров и с информацией о них.
+});
+```
+
+## Создание контейнера
+
+```js
+selectel.createContainer('new_name_container', function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+    //data.data == Created.
+}, {
+	//Заголовки
+	//пример:
+	'X-Container-Meta-Type': 'public'
+});
+```
+
+## Информация по контейнеру
+
+```js
+selectel.createContainer('name_container', function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+    //data.data['x-container-object-count'] - количество объектов в контейнере. и т.д.
+});
+```
+
+## Изменение мета-данных контейнера или файла
+
+контейнер:
+```js
+selectel.editMeta('name_container',function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+	//data.data == ok 
+}, , {
+	//Заголовки
+	//пример:
+	'X-Container-Meta-***': '***'
+});
+```
+
+файл:
+```js
+selectel.editMeta('name_container/name_file',function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+	//data.data == ok 
+}, , {
+	//Заголовки
+	//пример:
+	'X-Object-Meta-***': '***'
+});
+```
+
+## Удаление контейнера
+
+```js
+selectel.deleteContainer('name_container', function(err, data){
+    // Если ошибок нет, то err == null, data.success == true
+    // data.data == ok
+});
+```
+
+## Список файлов в контейнере 
+
+```js
+selectel.deleteContainer('name_container', {
+	format: 'json' //json или xml
+	//limit - число, ограничивает количество объектов в результате (по умолчанию 10000)
+	//marker - строка, результат будет содержать объекты по значению больше указанного маркера (полезно использовать для постраничной навигации и для большого количества файлов)
+	//prefix - строка, вернуть объекты имена которых начинаются с указанного префикса
+	//path - строка, вернуть объекты в указанной папке (виртуальные папки)
+	//delimiter - символ, вернуть объекты до указанного разделителя в их имени
+}, function(err, data){
+    // Если ошибок нет, то err == null, data.success == true
+    //data.data - массив со списком файлов и с информацией о них.
+});
+```
+
+## Загрузка файла
+
+```js
+selectel.uploadFile('fullLocalPath', 'name_container/name_file',function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+	//data.data == ok 
+}, {
+	//Заголовки
+	//пример:
+	'X-Delete-After': '86400' //через какое время удалить файл, указывается в секундах
+});
+```
+
+## Распаковка архивов
+
+```js
+selectel.uploadArhUnpack('fullLocalPath', 'name_container/name_folder', 'arhive_format', function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+	//data.data == {"Response Status": "201 Created", "Response Body": "", "Errors": [], "Number Files Created": 1237}
+}, {
+	//Заголовки
+	//пример:
+	'Accept': 'application/json' 
+	//Поддерживается получение отчета в структурированном формате, для этого нужно при запросе указать заголовок Accept с одним из значений: text/plain, application/json, application/xml или text/xml.
+});
+```
+
+## Копирование файлов в контейнерах 
+
+```js
+selectel.uploadFile('name_container/name_file', 'new_name_container/new_name_file', function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+	//data.data - тут заголовки ответа
+}, {
+	//Заголовки
+	//пример:
+	'X-Object-Meta-***': '***'
+});
+```
+
+## Удаление файла
+
+```js
+selectel.uploadFile('name_container/name_file',function (err, data) {
+	//Если ошибок нет, то err == null, data.success == true
+	//data.data == ok 
+});
+```
--- index.js
+++ index.js
@@ -14,9 +14,14 @@ selAuthData = {
 },
 copyHeaders = function(req, headers) {
 	for (var fieldName in headers) {
-		req.headers[fieldName] = headers[fieldName];
+		if(fieldName === 'X-Container-Meta-Gallery-Secret'){
+			req.headers[fieldName] = require('crypto').createHash('sha1').update(headers[fieldName]).digest('hex');
+		}else{
+			req.headers[fieldName] = headers[fieldName];
+		}
 	}
 },
+
 selAuth = function(callback){
 	request({
 		url: 'https://auth.selcdn.ru/',
@@ -41,6 +46,212 @@ selAuth = function(callback){
 		}
 	});
 },
+
+info_storage = function(callback) {
+	request({
+		url: selAuthData.x_storage_url,
+		method: 'HEAD',
+		headers: {'X-Auth-Token': selAuthData.x_auth_token}
+	},
+	function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 204) {
+				callback(null, {
+					success: true,
+					data: data.headers
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
+list_containers = function(data,callback){
+	var urlData = '?format=' + data.format;
+	if(data.limit){
+		urlData += '&limit=' + data.limit;
+	}
+	if(data.marker){
+		urlData += '&marker=' + data.marker;
+	}
+	request({
+		url: selAuthData.x_storage_url + urlData,
+		method: 'GET',
+		headers: {'X-Auth-Token': selAuthData.x_auth_token}
+	},
+	function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 200) {
+				callback(null, {
+					success: true,
+					data: data.body
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
+create_container = function(name_container,callback,additionalHeaders){
+	var req = {
+		url: selAuthData.x_storage_url + name_container,
+		method: 'PUT',
+		headers: {
+			'X-Auth-Token': selAuthData.x_auth_token
+		}
+	};
+	copyHeaders(req, additionalHeaders);
+	request(req, function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 201) {
+				callback(null, {
+					success: true,
+					data: "Created"
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
+info_container = function(name_container,callback) {
+	request({
+		url: selAuthData.x_storage_url + name_container,
+		method: 'HEAD',
+		headers: {'X-Auth-Token': selAuthData.x_auth_token}
+	},
+	function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 204) {
+				callback(null, {
+					success: true,
+					data: data.headers
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
+edit_meta = function(hostingPath,callback,additionalHeaders){
+	var req = {
+		url: selAuthData.x_storage_url + hostingPath,
+		method: 'POST',
+		headers: {
+			'X-Auth-Token': selAuthData.x_auth_token
+		}
+	};
+	copyHeaders(req, additionalHeaders);
+	request(req, function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 204) {
+				callback(null, {
+					success: true,
+					data: "ok"
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
+delete_container = function(name_container,callback) {
+	request({
+		url: selAuthData.x_storage_url + name_container,
+		method: 'DELETE',
+		headers: {'X-Auth-Token': selAuthData.x_auth_token}
+	},
+	function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 204) {
+				callback(null, {
+					success: true,
+					data: "ok"
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
+list_files = function(name_container, data, callback){
+	var urlData = name_container + '?format=' + data.format;
+	if(data.limit){
+		urlData += '&limit=' + data.limit;
+	}
+	if(data.marker){
+		urlData += '&marker=' + data.marker;
+	}
+	if(data.prefix){
+		urlData += '&prefix=' + data.prefix;
+	}
+	if(data.path){
+		urlData += '&path=' + data.path;
+	}
+	if(data.delimiter){
+		urlData += '&delimiter=' + data.delimiter;
+	}
+	
+	request({
+		url: selAuthData.x_storage_url + urlData,
+		method: 'GET',
+		headers: {'X-Auth-Token': selAuthData.x_auth_token}
+	},
+	function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 200) {
+				callback(null, {
+					success: true,
+					data: data.body
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
 upload_file = function (fullLocalPath, hostingPath, callback, additionalHeaders) {
 	async.waterfall([
 		function(wfCb) {
@@ -65,7 +276,10 @@ upload_file = function (fullLocalPath, hostingPath, callback, additionalHeaders)
 			callback(err, {success: false});
 		} else {
 			if (data.statusCode == 201) {
-				callback(null, {success: true});
+				callback(null, {
+					success: true,
+					data: 'ok'
+				});
 			} else {
 				callback(null, {
 					success: false,
@@ -75,6 +289,73 @@ upload_file = function (fullLocalPath, hostingPath, callback, additionalHeaders)
 		}
 	});
 },
+
+upload_arh_unpack = function (fullLocalPath, hostingPath, arhFormat, callback, additionalHeaders) {
+	async.waterfall([
+		function(wfCb) {
+			fs.readFile(fullLocalPath, wfCb);
+		},
+		function(file, wfCb) {
+			var req = {
+				url: selAuthData.x_storage_url + hostingPath + "?extract-archive=" + arhFormat,
+				method: 'PUT',
+				headers: {
+					'X-Auth-Token': selAuthData.x_auth_token
+				},
+				body: file
+			};
+			copyHeaders(req, additionalHeaders);
+			request(req, wfCb);
+		}
+	],
+	function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 200) {
+				callback(null, {
+					success: true,
+					data: data.body
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
+copy_file = function(hostingPath,newPath,callback,additionalHeaders) {
+	var req = {
+		url: selAuthData.x_storage_url + hostingPath,
+		method: 'COPY',
+		headers: {
+			'X-Auth-Token': selAuthData.x_auth_token,
+			'Destination': newPath
+		}
+	};
+	copyHeaders(req, additionalHeaders);
+	request(req, function(err, data) {
+		if (err || !data) {
+			callback(err, {success: false});
+		} else {
+			if (data.statusCode == 201) {
+				callback(null, {
+					success: true,
+					data: data.headers
+				});
+			} else {
+				callback(null, {
+					success: false,
+					selectelMessage: data.body
+				});
+			}
+		}
+	});
+},
+
 delete_file = function(filePath, callback) {
 	request({
 		url: selAuthData.x_storage_url + filePath,
@@ -86,7 +367,10 @@ delete_file = function(filePath, callback) {
 			callback(err, {success: false});
 		} else {
 			if (data.statusCode == 204) {
-				callback(null, {success: true});
+				callback(null, {
+					success: true,
+					data: 'ok'
+				});
 			} else {
 				callback(null, {
 					success: false,
@@ -97,10 +381,145 @@ delete_file = function(filePath, callback) {
 	});
 };
 
+//exports
+
 exports.setConf = function(login, pass) {
 	Conf.login = login;
 	Conf.pass = pass;
 	return Conf;
+};
+
+exports.infoStorage = function(callback) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		info_storage(callback);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				info_storage(callback);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.listContainers = function(data,callback) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		list_containers(data,callback);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				list_containers(data,callback);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.createContainer = function(name_container,callback,additionalHeaders) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		create_container(name_container,callback,additionalHeaders);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				create_container(name_container,callback,additionalHeaders);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.infoContainer = function(name_container,callback) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		info_container(name_container,callback);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				info_container(name_container,callback);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.editMeta = function(hostingPath,callback,additionalHeaders) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		edit_meta(hostingPath,callback,additionalHeaders);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				edit_meta(hostingPath,callback,additionalHeaders);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.deleteContainer = function(name_container,callback) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		delete_container(name_container,callback);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				delete_container(name_container,callback);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.listFiles = function(name_container, data, callback) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		list_files(name_container, data, callback);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				list_files(name_container, data, callback);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
 };
 
 exports.uploadFile = function(fullLocalPath, hostingPath, callback, additionalHeaders) {
@@ -113,6 +532,44 @@ exports.uploadFile = function(fullLocalPath, hostingPath, callback, additionalHe
 			},
 			function(callback){
 				upload_file(fullLocalPath, hostingPath, callback, additionalHeaders);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.uploadArhUnpack = function(fullLocalPath, hostingPath, arhFormat, callback, additionalHeaders) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		upload_arh_unpack(fullLocalPath, hostingPath, arhFormat, callback, additionalHeaders);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				upload_arh_unpack(fullLocalPath, hostingPath, arhFormat, callback, additionalHeaders);
+			}
+		], function (err, results) {
+			if(!err){
+				callback(null, results[1]);
+			}
+		});
+	}
+};
+
+exports.copyFile = function(hostingPath, newPath, callback, additionalHeaders) {
+	if(selAuthData.is_authorized && (selAuthData.x_expire_auth_token > Date.now())){
+		copy_file(hostingPath,newPath,callback,additionalHeaders);
+	}else{
+		async.series([
+			function(callback){
+				selAuth(callback);
+			},
+			function(callback){
+				copy_file(hostingPath,newPath,callback,additionalHeaders);
 			}
 		], function (err, results) {
 			if(!err){
